0d018b0e5d78fd684933c3ab55d7a9590a5681ca
Dan Short <44993+danshort@users.noreply.github.com>
2023-12-13 19:42:05 -0600

    Getting started with Day 2

---
+++ Day2/day2-part1.js
@@ -0,0 +1,15 @@
+let aoc = require("../aoc");
+let string = require("fs").readFileSync("./day2/day2data-sample.txt", 'utf8')
+
+let lines = aoc.convertToLines(string);
+
+let runningTotal = 0;
+
+// Loop through each game and test it's validity
+
+
+
+function isGameValid(game){
+
+    return false;
+}
